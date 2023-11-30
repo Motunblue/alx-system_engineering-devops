df89aa074db9af1c5b048f520f31b632ad1a36b7
Motun <ajirotutu4real@gmail.com>
2023-11-30 03:13:53 +0100

    Added task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# Install nginx listening on port 80
+# Get / returns a page that with content hello world
+
+exec {'install':
+  provider => shell,
+  command  => 'apt-get -y update; apt-get -y install nginx; ufw allow "Nginx HTTP"',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => Hello World!,
+}
+
+file_line {'redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;'
+  line   => 'rewrite ^/redirect_me https://google.com permanent;'
+}
+
+service ( 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
