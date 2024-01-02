79ccc0bb38e6b21370890eb967854f2771abfe9f
Motun <ajirotutu4real@gmail.com>
2024-01-02 19:13:33 +0100

    Modified task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+"""Get information about employee ID
+    argv[1]: employee id
+"""
 import requests
 from sys import argv
 
 
-"""Get information about employee ID
-    argv[1]: employee id
-"""
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
 
-url = "https://jsonplaceholder.typicode.com/"
+    res = requests.get("{}users/{}".format(url, argv[1]))
+    name = res.json().get("name")
 
-res = requests.get("{}users/{}".format(url, argv[1]))
-name = res.json().get("name")
+    res = requests.get("{}todos/".format(url))
+    tasks = res.json()
+    task_count = 0
+    completed = 0
+    task_title = []
+    for task in tasks:
+        if task.get("userId") == int(argv[1]):
+            task_count += 1
+            if task.get("completed") is True:
+                completed += 1
+                task_title.append(task.get("title"))
 
-res = requests.get("{}todos/".format(url))
-tasks = res.json()
-task_count = 0
-completed = 0
-task_title = []
-for task in tasks:
-    if task.get("userId") == int(argv[1]):
-        task_count += 1
-        if task.get("completed") is True:
-            completed += 1
-            task_title.append(task.get("title"))
-
-print("Employee {} is done with tasks({}/{}):\n\t".format(
-    name, completed, task_count),
-      "\n\t ".join(task_title))
+    print("Employee {} is done with tasks({}/{}):\n\t".format(
+        name, completed, task_count),
+        "\n\t ".join(task_title))
