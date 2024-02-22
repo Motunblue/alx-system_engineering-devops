d0c1ffda2e4639e13a709258cb281c11d3c85134
Motunrayo <ajirotutu4real@gmail.com>
2024-02-22 15:52:15 +0100

    Added files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Fix request failures on high volume requests
 
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+exec { 'replace_ulimit':
+  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  notify  => Exec['restart_nginx'],
 }
 
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+exec { 'restart_nginx':
+  command  => 'sudo service nginx restart',
+  refreshonly => true,
 }
 
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Fix open file without error.
+
+exec { 'increase-nofile-limit':
+  command  => 'sudo sed -i -e "s/nofile 5/nofile 50000/" -e "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+  refreshonly => true,
+  subscribe => File['/etc/security/limits.conf'],
+}
+
+file { '/etc/security/limits.conf':
+  ensure => present,
+  notify => Exec['increase-nofile-limit'],
+}
+
