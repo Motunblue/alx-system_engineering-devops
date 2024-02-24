6f8ef2a48d07b2a616e5b61291ffd1bbd8061962
Motunrayo <ajirotutu4real@gmail.com>
2024-02-24 08:33:37 +0100

    updated 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Requery reddit api for number of subscibers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "(Windows NT 10.0; Win64; x64)"}
+    headers = {"User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/motun)"}
     response = requests.get(url, headers=headers, allow_redirect=False)
     if response.status_code == 404:
         return 0
