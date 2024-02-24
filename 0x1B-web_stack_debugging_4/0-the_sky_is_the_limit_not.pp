6a5241ce81ca04b81e6d24861d063b9b10b7f453
Motunrayo <ajirotutu4real@gmail.com>
2024-02-24 08:11:54 +0100

    Modified

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Fix request failures on high volume requests
 
-exec { 'replace_ulimit':
-  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  notify  => Exec['restart_nginx'],
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-exec { 'restart_nginx':
-  command     => 'sudo service nginx restart',
-  refreshonly => true,
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
-
